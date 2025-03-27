867b7573b2d4b55a2945cbd3d2757f13528df6e1
AYOUB BOUSSAIDI <Boussaidi-Ayoub@hotmail.com>
2025-03-27 22:15:24 +0000

    add more version features

---
+++ C#13.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace project_echallenge
+{
+    internal class C_13
+    {
+        // New 'Lock' type for thread synchronization
+        private readonly Lock _syncLock = new Lock();
+
+        // Enhanced 'params' collections
+        public void DisplayItems(params List<string> items)
+        {
+            Console.WriteLine("Items:");
+            foreach (var item in items)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+
+        // New escape sequence '\e' for ESC character
+        public void DisplayEscapeSequence()
+        {
+            string message = $"This is an escape sequence: \e[1;31mRed Text\e[0m";
+            Console.WriteLine(message);
+        }
+
+        // Implicit indexer access in object initializers
+        public void InitializeArray()
+        {
+            var numbers = new int[5];
+            numbers[^1] = 10; // Sets the last element to 10
+            Console.WriteLine($"Last number: {numbers[^1]}");
+        }
+
+        // Method group natural type improvements
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void InvokeShowMessage()
+        {
+            Action<string> messageAction = ShowMessage;
+            messageAction("Method group natural type improvement in action.");
+        }
+
+        // 'ref' locals and unsafe contexts in iterators and async methods
+        public async Task ProcessDataAsync()
+        {
+            Span<byte> buffer = stackalloc byte[1024];
+            await Task.Yield();
+            // Additional processing
+        }
+
+        // 'ref struct' types implementing interfaces
+        public void UseRefStruct()
+        {
+            var spanWrapper = new SpanWrapper(new Span<byte>(new byte[10]));
+            spanWrapper.Process();
+        }
+
+        // Partial properties and indexers
+        public partial class PartialClass
+        {
+            public partial string Name { get; set; }
+        }
+
+        // Overload resolution priority
+        public void DemonstrateOverloadResolution()
+        {
+            Console.WriteLine("Overload resolution priority demonstration.");
+        }
+
+        [OverloadResolutionPriority(1)]
+        public void DemonstrateOverloadResolution(int number)
+        {
+            Console.WriteLine($"Overload with int: {number}");
+        }
+    }
+}
---
+++ C#14.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+// File-scoped using directive
+using System.IO;
+
+namespace project_echallenge
+{
+    internal class C_14
+    {
+        // Field-backed property
+        public int Value
+        {
+            get => @field;
+            set => @field = value < 0 ? 0 : value;
+        }
+        private int field;
+
+   
+
+    // Target-typed conditional expression
+    public void DisplayMessage(bool isSuccess)
+    {
+        string message = isSuccess is true ? "Operation succeeded." : "Operation failed.";
+        Console.WriteLine(message);
+    }
+
+    // Partial constructor
+    public partial class SampleClass
+    {
+        public SampleClass() : this(0) { }
+        public SampleClass(int value) { }
+    }
+
+    // Partial event
+    public partial class EventPublisher
+    {
+        public partial event EventHandler<EventArgs> DataProcessed;
+    }
+
+    // Using 'nameof' with unbound generics
+    public void DisplayTypeName()
+        {
+            Console.WriteLine(nameof(List<>)); // Outputs: List<>
+        }
+
+        // File-local using directive
+        // using System.IO; // Uncomment to use within this file
+    }
+}
+
